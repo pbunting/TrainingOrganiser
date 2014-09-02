f02f85c2ac0881bedde7d377e05f421c2c0ee76b
Paul Bunting <paulbunting@pauls-imac.bigpond>
2014-09-03 06:18:23 +1000

    Initial commit

---
+++ TrainingOrganiser/OHTProfileViewController.h
@@ -0,0 +1,14 @@
+//
+//  OHTProfileViewController.h
+//  TrainingOrganiser
+//
+//  Created by Paul Bunting on 2/09/2014.
+//  Copyright (c) 2014 100 Trees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OHTProfileViewController : UIViewController
+- (IBAction)pressed:(id)sender;
+
+@end
